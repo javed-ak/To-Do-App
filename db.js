87ad5ad714fad0e7f8e282679740d31a8585bf1d
Javed Akhtar <javedakhtar@Javeds-MacBook-Air.local>
2024-08-12 16:11:52 +0530

    Project Modified

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://admin:adminkapassword@cluster0.yojdofd.mongodb.net/todos");
 
-const todoSchema = mongoose.Schema({
+const todoSchema = mongoose.Schema({ 
     title: String,
     description: String,
     completed: Boolean
